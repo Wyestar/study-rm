94d0123d3deb15e40f55a13b309cf93bf9fc3906
Wyestar <dougmchin@gmail.com>
2018-02-12 11:11:58 -0500

    string
    sort practice

---
+++ sortCharactersInAWord.js
@@ -0,0 +1,37 @@
+function nico(key, message) {
+
+  let alphabet = "!abcdefghijklmnopqrstuvwxyz";
+
+  function sortStr(str) {
+    return a - b;
+  }
+
+  function sorter(str) {
+    return key.split('').sort().join('');
+  }
+
+  let sortedKey = sorter(key);
+  console.log(sortedKey);
+
+  let numberKeyCode = "";
+  let sortedArray = [];
+  let keyObj = {};
+  for (let i = 0; i < key.length; i++) {
+    keyObj[key[i]] = alphabet.indexOf(key[i]);
+  }
+  for (var char in keyObj) {
+    sortedArray.push([char, keyObj[char]]);
+  }
+  
+  // sortedArray may not be completely sorted
+  let result = sortedArray.sort(function(a, b) {
+    return a[1] - b[1];
+  });
+  console.log(sortedArray ,'sortedArray');
+  console.log(result);
+  
+  return result;
+}
+
+
+nico('czrazy', 'message');
