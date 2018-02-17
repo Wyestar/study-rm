d0b17796cb885a03f671053d4eeeeff9be812510
Wyestar <dougmchin@gmail.com>
2018-02-17 14:51:20 -0500

    subsequence
    prompt

---
+++ ,agicvowelSubsequence.js
@@ -0,0 +1,7 @@
+/*A magical sub-sequence of a string S is a sub-sequence of S that contains all five vowels in order. Find the length of largest magical sub-sequence of a string S.
+
+For example, if S = aeeiooua, then aeiou and aeeioou are magical sub-sequences but aeio and aeeioua are not.
+
+other test cases:
+S = "aeiouaaeeaiioooooieuau" => aaeeiiooooouu is longest subsequence. return 13
+*/
