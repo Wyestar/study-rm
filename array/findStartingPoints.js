8ca3adae0908ed3f224044aee6f6809b22f1db4f
Wyestar <food.bolus@gmail.com>
2018-02-27 17:14:54 -0500

    array startingpoints

--- array/findStartingPoints.js
+++ array/findStartingPoints.js
@@ -4,8 +4,33 @@
 // [1, 5, 7, 11]
 
 function startingPoints(arr) {
+  let storage = [];
+  // can put arr[0] in storage immediately
 
-    return [];
+  let pointer = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    pointer = arr[i];
+
+    for (let j = i+1; j < arr.length; j++) {
+      console.log(pointer);
+
+      if (arr[j] === (pointer + 1)) {
+        pointer = arr[j];
+      }
+      else {
+        // consecutive block is broken
+        // reset pointer
+        // reset i
+        storage.push(arr[i]);
+        i = j;
+      }
+    }
+    // console.log(pointer);
+  }
+  console.log(storage ,'storage at end');
+
+  return [];
 }
 
 let array = [1, 2, 3, 5, 7, 8, 9, 11, 12];
