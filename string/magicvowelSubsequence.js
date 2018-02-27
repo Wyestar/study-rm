cd91a5902c91297f7dc07118dc36644f4a70be3e
Wyestar <food.bolus@gmail.com>
2018-02-27 16:39:51 -0500

    string magicss

--- string/magicvowelSubsequence.js
+++ string/magicvowelSubsequence.js
@@ -10,40 +10,37 @@ S = "aeiouaaeeaiioooooieuau" => aaeeiiooooou is longest subsequence. return 13
 // for each vowel, find largest consecutive block
 // if they are in order, add lengths
 // find all possible magic subsequences, and store them
-// return the length of the longest one 
+// return the length of the longest one
+// find all indices of 'a'
+// ignore consecutive 'a', only record start of 'a' blocks
 
 // after 'a', if every block of each vowel is stored, the order of the blocks is important.
 // magicss can also start from a later 'a' block
 // from each valid partial magicss, build all possible complete magicss
-// find length of longest complete 
+// find length of longest complete
 // store all permutations in an object?
 
 // longest increasing subsequence
 // there are potentially many LIS in string, only concerned about the longest
+// can check lengths as they are built and only store one LIS length
 
 // assuming input is only lowercase vowels
 
 function magic(str, key = 0) {
 	console.log(key ,'key');
 	console.log(str);
-	
+
 	vowels = ["a", "e", "i", "o", "u"];
-	
+
+	idxStorage = [];
 	temp = "";
 	for (i = 0; i < str.length; i++) {
-	  if (vowels.indexOf(str[i]) === key) {
-	    // find all consecutive 'a's if there are any
-	    // otherwise, start searching and counting 'e'
-	    temp = str.slice(i + 1);
-	    if (key === 5) {
-	      key = -1;
-	    }
-	    key = key + 1;
-	    // callback with temp and next key (vowel)
-	    magic(temp, key);
+	  if (str[i] === "a") {
+			idxStorage.push(i);
 	  }
 	}
-	
+	console.log(idxStorage, 'idxStorage end of loop');
+
 	return null;
 }
 
