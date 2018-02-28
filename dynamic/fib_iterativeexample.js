87bd44a5bfe7f9ccfa856a69400d84af497e922d
Wyestar <food.bolus@gmail.com>
2018-02-28 12:40:05 -0500

    dynamic fib

---
+++ dynamic/fib_iterativeexample.js
@@ -0,0 +1,13 @@
+function fibDynamic(n) {
+  let fibNumbers = [0, 1];
+
+  for (var j = 2; j <= n; j++) {
+    fibNumbers[j] = fibNumbers[j - 1] + fibNumbers[j - 2];
+  }
+
+  return fibNumbers[n];
+}
+
+fibDynamic(1476);
+// first 1 is the first fibonacci number
+// 0, 1, 1, 2, 3, 5, 8
